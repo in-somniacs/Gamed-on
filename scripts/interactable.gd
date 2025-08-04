709a7b6781b6f9cf65dd462ac9077551c16a9b5d
Masuwuked <74668157+Masuwuked@users.noreply.github.com>
2025-08-05 00:14:19 +0530

    init

---
+++ scripts/interactable.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+@onready var player: CharacterBody2D = $"../player"
+@onready var hint: RichTextLabel = $hint
+
+var interatable = false
+func _process(delta: float) -> void:
+	if interatable == true && Input.is_action_just_pressed("interact"):
+		print("interacted")
+
+
+func _on_body_entered(body: Node2D) -> void:
+	interatable = true
+	hint.visible = true
+
+
+func _on_body_exited(body: Node2D) -> void:
+	interatable = false
+	hint.visible = false
---
+++ scripts/npc_zone.gd
@@ -0,0 +1,6 @@
+extends Area2D
+const PLAYER = preload("res://scenes/player.tscn")
+
+
+func _on_body_entered(body: Node2D) -> void:
+	print("hello :)")
