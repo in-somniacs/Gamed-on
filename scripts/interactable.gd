763b015b9588fec0c79ee3a07ad9114fa6bba287
Masuwuked <74668157+Masuwuked@users.noreply.github.com>
2025-08-05 19:18:12 +0530

    testing

--- scripts/interactable.gd
+++ scripts/interactable.gd
@@ -22,7 +22,8 @@ func _process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":  # Only allow Player to trigger
 		interactable = true
-		hint.visible = true
+		hint.visible = true 
+		
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
