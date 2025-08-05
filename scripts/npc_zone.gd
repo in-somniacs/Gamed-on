9dc11f79d6be0d8a39fdfef609db8a3a5f1814a2
Masuwuked <74668157+Masuwuked@users.noreply.github.com>
2025-08-05 18:21:22 +0530

    llsa

--- scripts/npc_zone.gd
+++ scripts/npc_zone.gd
@@ -3,4 +3,8 @@ const PLAYER = preload("res://scenes/player.tscn")
 
 
 func _on_body_entered(body: Node2D) -> void:
+<<<<<<< Updated upstream
 	print("hello :)")
+=======
+	pass
+>>>>>>> Stashed changes
