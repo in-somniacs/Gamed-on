12191b08be37dd13fe28b870fe19f2c6997ab2ba
Masuwuked <74668157+Masuwuked@users.noreply.github.com>
2025-08-05 19:11:05 +0530

    dont touch stashed again ;-;

--- scripts/interactable.gd
+++ scripts/interactable.gd
@@ -1,42 +1,30 @@
 extends Area2D
-@onready var hint: RichTextLabel = $hint
-@onready var player: CharacterBody2D = $"../Player"
-@onready var floor_0: Node2D = $"../stairs/floor_0"
-@onready var floor_1: Node2D = $"../stairs/floor_1"
+@onready var hint: RichTextLabel = $RichTextLabel
 
-@onready var floor_0: Node2D = $"../teleportpoints/floor_0"
-@onready var floor_1: Node2D = $"../teleportpoints/floor_1"
 
-var floor0: Vector2
-var floor1: Vector2
+@onready var player: CharacterBody2D = get_node("../Player")
+@onready var floor_0: Node2D = get_node("../stairs/floor_0")
+@onready var floor_1: Node2D = get_node("../stairs/floor_1")
+
 var interactable = false
 
 func _ready():
-	floor0 = floor_0.global_position
-	floor1 = floor_1.global_position
+	# Debug: Make sure the nodes are found correctly
+	print("Player:", player)
+	print("Floor 0:", floor_0)
+	print("Floor 1:", floor_1)
 
-<<<<<<< Updated upstream
 func _process(delta: float) -> void:
 	if interactable and Input.is_action_just_pressed("interact"):
-		player.global_position = floor1  # or floor0 depending on your logic
-		print("Teleported to:", floor1)
-=======
-
-var interatable = false
-func _ready() -> void:
-	pass	
-	
-func _process(delta: float) -> void:
-	var floor1 = floor_1.global_position
-	var floor0 = floor_0.global_position
-	if interatable == true && Input.is_action_just_pressed("interact"):
-		print(floor1)
->>>>>>> Stashed changes
+		player.global_position = floor_1.global_position
+		print("Teleported to:", floor_1.global_position)
 
 func _on_body_entered(body: Node2D) -> void:
-	interactable = true
-	hint.visible = true
+	if body.name == "Player":  # Only allow Player to trigger
+		interactable = true
+		hint.visible = true
 
 func _on_body_exited(body: Node2D) -> void:
-	interactable = false
-	hint.visible = false
+	if body.name == "Player":
+		interactable = false
+		hint.visible = false
