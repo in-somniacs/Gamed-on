f6b5c6f04501ac32e630c62d66f7bba012b62928
a6h1 <abhi.r1509.git@gmail.com>
2025-08-07 19:17:35 +0530

    dialogic

---
+++ scenes/rooms/room_0.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _input(event: InputEvent) -> void:
+	if Dialogic.current_timeline != null:
+		return
+	
+	if event is InputEventKey and event.keycode == KEY_ENTER and event.pressed:
+		Dialogic.start("res://timelines/roomspawntimeline.dtl")
+		get_viewport().set_input_as_handled()
+		
+		
+		
+		
+		
