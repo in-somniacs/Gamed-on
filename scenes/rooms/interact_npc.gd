b619ce24f44f3f5be123347cd12d4394e42d607b
a6h1 <abhi.r1509.git@gmail.com>
2025-08-08 03:11:56 +0530

    mera khel khatam

--- scenes/rooms/interact_npc.gd
+++ scenes/rooms/interact_npc.gd
@@ -3,27 +3,46 @@ extends Node2D
 
 @export var player: CharacterBody2D 
 @export var dialogue: String
-@export var textholder: RichTextLabel
+@onready var chatbox: CollisionShape2D = $Chatdetection/chatbox
+@onready var res_chat: CollisionPolygon2D = $respawn_chat/res_chat
 
-var interact = false
+
+var player_in_area = false
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	textholder.visible = false
-	textholder.text = dialogue
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if interact && Input.is_action_just_pressed("interact"):
-		textholder.visible = true
+	if player_in_area:
+		if Input.is_action_pressed("dialogic_default_action"):
+			run_dialogue("res://timelines/footpathtimeline.dtl")
+			chatbox.disabled = true
+			
+			
+			
+func _on_chatdetection_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_area = true
 		
-	
-	
-func _on_body_entered(body: Node2D) -> void:
-	
-	interact = true
+func _on_chatdetection_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_area = false 
+		
+		chatbox.disabled = false
+		
+		
+		
 
-
-func _on_body_exited(body: Node2D) -> void:
-	interact = false
+func run_dialogue(dialogue_string):
+	Dialogic.start(dialogue_string)
 	
+
+func _on_respawn_chat_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		pass
+		
+func _on_respawn_chat_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
