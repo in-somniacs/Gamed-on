4d6f1298892618979ee106e6149f51b38f5185ae
a6h1 <abhi.r1509.git@gmail.com>
2025-08-08 22:17:41 +0530

    footpath scene complete with console animation

---
+++ scenes/tachyon/scenes/mail_box.gd
@@ -0,0 +1,46 @@
+extends CharacterBody2D
+
+
+@export var player: CharacterBody2D 
+
+@onready var chatbox: CollisionShape2D = $Chatdetection/chatbox
+#@onready var res_chat: CollisionPolygon2D = $respawn_chat/res_chat
+
+
+var player_in_area = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if player_in_area:
+		if Input.is_action_pressed("dialogic_default_action"):
+			run_dialogue("res://timelines/consoleget.dtl")
+			chatbox.disabled = true
+			
+			
+			
+func _on_chatdetection_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_area = true
+		
+func _on_chatdetection_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_area = false 
+		
+		
+		
+
+func run_dialogue(dialogue_string):
+	Dialogic.start(dialogue_string)
+	
+
+func _on_respawn_chat_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		pass
+		
+func _on_respawn_chat_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
