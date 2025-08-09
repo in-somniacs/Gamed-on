9b9b2a104145c52df63ef3d8b427b3cb76293eb5
Masuwuked <74668157+Masuwuked@users.noreply.github.com>
2025-08-10 02:57:50 +0530

    klmlkm

--- scripts/slot.gd
+++ scripts/slot.gd
@@ -2,6 +2,10 @@ extends Panel
 
 @onready var backdrop: Sprite2D = $backdrop
 @onready var item_: Sprite2D = $CenterContainer/Panel/item
+@onready var tooltip_label: Label = $Label
+var INVENTORY = preload("res://items/inventory.tres")
+var inv = preload("res://scenes/ui/invenory.tscn")
+var item = preload("res://items/ps.tres")
 
 func update(item: inventoryItem):
 		if !item:
@@ -11,4 +15,16 @@ func update(item: inventoryItem):
 			backdrop.frame = 1
 			item_.visible = true
 			item_.texture = item.texture
+func _ready() -> void:
+	pass
 			
+func _on_mouse_entered() -> void:
+	if InventoryItem:
+		print()
+		tooltip_label.text = "[b]%s[/b]\n%s" % [item.title,  item.info]
+		tooltip_label.visible = true
+		tooltip_label.global_position = get_global_mouse_position() + Vector2(16, 16)
+
+
+func _on_mouse_exited() -> void:
+	tooltip_label.visible = false
