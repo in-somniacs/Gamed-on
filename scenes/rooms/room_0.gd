253be54c1cfc94eb4ac6a2ab20f925f9a326a6cb
a6h1 <abhi.r1509.git@gmail.com>
2025-08-11 02:43:23 +0530

    player movement restriction

--- scenes/rooms/room_0.gd
+++ scenes/rooms/room_0.gd
@@ -10,10 +10,15 @@ func _ready() -> void:
 	if not global.has_played_room_intro:
 		global.canmove = false
 		global.has_played_room_intro = true
+
 		await get_tree().create_timer(5.0).timeout
-		Dialogic.start("res://timelines/roomentry1.dtl")
-		global.canmove = true
-		
+
+		var dialog_node = Dialogic.start("res://timelines/roomentry1.dtl")
+		dialog_node.connect("timeline_end", Callable(self, "_on_dialogue_finished"))
+
+func _on_dialogue_finished():
+	global.canmove = true
+
 
 
 
