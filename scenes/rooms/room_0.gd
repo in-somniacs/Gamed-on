eea8b10e1cf9acdbc1bb51d0fd687bee309dd21f
a6h1 <abhi.r1509.git@gmail.com>
2025-08-11 01:18:09 +0530

    uh

--- scenes/rooms/room_0.gd
+++ scenes/rooms/room_0.gd
@@ -7,7 +7,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	await get_tree().create_timer(5.0).timeout
+	Dialogic.start("res://timelines/roomentry1.dtl")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
