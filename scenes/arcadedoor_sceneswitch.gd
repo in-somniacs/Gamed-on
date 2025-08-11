b73ee287028ff54fc2fa811ca5e79d32e06db322
Masuwuked <74668157+Masuwuked@users.noreply.github.com>
2025-08-12 02:54:15 +0530

    da

--- scenes/arcadedoor_sceneswitch.gd
+++ scenes/arcadedoor_sceneswitch.gd
@@ -3,6 +3,7 @@ extends Area2D
 @export var player: CharacterBody2D 
 @export var next_scene: String 
 @export var popup: NinePatchRect
+@export var box: CollisionShape2D
 
 
 
@@ -10,7 +11,7 @@ var interactable = false
 
 func _ready():
 	global.new_scene_placement = next_scene 
-
+	box.disabled = false
 	print("Player:", player)
 
 
@@ -26,6 +27,7 @@ func _process(delta: float) -> void:
 			#get_tree().change_scene_to_file(next_scene)
 	elif interactable and Input.is_action_just_pressed("interact"):
 		Dialogic.start("res://timelines/locked_door.dtl")
+		box.disabled = true
 		
 
 func _on_body_entered(body: Node2D) -> void:
