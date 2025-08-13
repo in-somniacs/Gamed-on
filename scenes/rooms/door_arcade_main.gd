974ec787a20d10e019d3b570cb7b3135a91a3f4f
Masuwuked <74668157+Masuwuked@users.noreply.github.com>
2025-08-13 15:03:23 +0530

    lmao

--- scenes/rooms/door_arcade_main.gd
+++ scenes/rooms/door_arcade_main.gd
@@ -4,13 +4,16 @@ extends Area2D
 @export var player: CharacterBody2D
 @export var popup: NinePatchRect
 @export var destination: Node2D
+@export var ani: AnimationPlayer
 var player_in_area = false
 
 func _ready() -> void:
 			global.teleportdoor = false
 func _physics_process(delta: float) -> void:
 		if player_in_area == true && Input.is_action_pressed("dialogic_default_action") && global.teleportdoor == true:
+			ani.play("fade_in")
 			player.global_position = destination.global_position
+			ani.play("fade_out")
 
 
 func _on_body_entered(body: Node2D) -> void:
