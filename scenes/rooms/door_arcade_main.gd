f2a3f70ea21a40aa74a27b7e7bcc4e566cd0e4a3
a6h1 <abhi.r1509.git@gmail.com>
2025-08-13 15:24:13 +0530

    collisions

--- scenes/rooms/door_arcade_main.gd
+++ scenes/rooms/door_arcade_main.gd
@@ -1,9 +1,10 @@
 extends Area2D
 
-@export var dialog: String
+@export var dialogue: String
 @export var player: CharacterBody2D
 @export var popup: NinePatchRect
 @export var destination: Node2D
+@onready var chatbox: CollisionShape2D = $Chatdetection/chatbox
 var player_in_area = false
 
 func _ready() -> void:
@@ -11,7 +12,9 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 		if player_in_area == true && Input.is_action_pressed("dialogic_default_action") && global.teleportdoor == true:
 			player.global_position = destination.global_position
-
+			run_dialogue(dialogue)
+			chatbox.disabled = true
+			global.canmove = false
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.has_method("player"):
@@ -22,3 +25,26 @@ func _on_body_exited(body: Node2D) -> void:
 	if body.has_method("player"):
 		popup.visible = false
 		player_in_area = false
+
+func run_dialogue(dialogue_string):
+	# Enable glitch
+	#shader_mesh.material.set_shader_parameter("glitch_enabled", true)
+	
+	# Play glitch sound alongside dialogue
+	#glitch_sfx.stop()
+	#glitch_sfx.play()
+
+	# Start dialogue immediately
+	Dialogic.start(dialogue_string)
+	Dialogic.timeline_ended.connect(_on_dialogue_end)
+	
+	# When sound finishes, stop glitch effect
+	#await get_tree().create_timer(10.0).timeout
+	#shader_mesh.material.set_shader_parameter("glitch_enabled", false)
+	
+
+func _on_dialogue_end():
+	#shader_mesh.material.set_shader_parameter("glitch_enabled", false)
+	Dialogic.timeline_ended.disconnect(_on_dialogue_end)
+	global.canmove = true
+	
