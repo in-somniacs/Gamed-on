af7b75f1a8ea4b9962bcf5548cbf90eb56243d0c
Masuwuked <74668157+Masuwuked@users.noreply.github.com>
2025-08-14 21:18:43 +0530

    lmao

--- scenes/tachyon/scenes/finale.gd
+++ scenes/tachyon/scenes/finale.gd
@@ -2,10 +2,15 @@ extends Node2D
 
 @export var glitch_material: MeshInstance2D
 @export var dialogue: String
-
+@export var jump: Panel
+@export var aldi: AudioStreamPlayer2D
+@export var ani: AnimatedSprite2D
 var player_in_area = false
 
+
 func _ready() -> void:
+	jump.visible = false
+	aldi.stream = preload("res://assets/sfx/jump_me_daddy.mp3")
 	# Make sure glitch is off at start (optional — remove if Dialogic handles it fully)
 	global.glitch_enabled = false
 
@@ -26,6 +31,8 @@ func run_dialogue():
 
 func _on_dialogue_end():
 	Dialogic.timeline_ended.disconnect(_on_dialogue_end)
+	ani.play("death")
+	global.disable_player_movement()
 
 func _on_static_body_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player" and not player_in_area:
